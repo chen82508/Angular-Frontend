ebae1db63267d7c5a3e2a05c29e9fa0553efad74
chen82508 <31638921+chen82508@users.noreply.github.com>
2022-11-01 09:41:16 +0800

    Fix navigations spec file.

--- src/core/navigations/navigations.spec.ts
+++ src/core/navigations/navigations.spec.ts
@@ -1,21 +1,13 @@
 import { TestBed } from '@angular/core/testing';
-import {
-  PerfectScrollbarConfigInterface,
-  PerfectScrollbarModule,
-} from 'ngx-perfect-scrollbar';
 import { AppHeaderComponent } from '@core-nav/components/app-header/app-header.component';
 import { AppNavbarComponent } from '@core-nav/components/app-navbar/app-navbar.component';
 
 describe('AppNavbarComponent', () => {
-  const PS_CONFIG_FOR_TEST: PerfectScrollbarConfigInterface = {
-    wheelPropagation: true,
-  };
-
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AppHeaderComponent, AppNavbarComponent],
-      imports: [PerfectScrollbarModule],
-      providers: [{ provide: 'PS_CONFIG', useValue: PS_CONFIG_FOR_TEST }],
+      imports: [],
+      providers: [],
     }).compileComponents();
   });
 
