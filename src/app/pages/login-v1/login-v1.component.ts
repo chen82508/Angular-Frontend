d39576bef3b66ce124d2cdcc7f618289b4f406da
chen82508 <chen82508@gmail.com>
2022-11-03 22:38:13 +0800

    Do not show password meter in login page.

--- src/app/pages/login-v1/login-v1.component.ts
+++ src/app/pages/login-v1/login-v1.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+// import { BrowserModule } from "@angular/platform-browser";
+// import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { FormsModule } from "@angular/forms";
 import { ImageModule } from 'primeng/image';
 import { InputTextModule } from "primeng/inputtext";
